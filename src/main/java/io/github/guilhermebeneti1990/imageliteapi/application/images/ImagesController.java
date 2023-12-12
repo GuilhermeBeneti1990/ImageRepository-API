9e8f6a315951af0fd8bd48e610a64b4a29b72660
GuilhermeBeneti1990 <guilhermebeneti@icloud.com>
2023-12-12 12:00:23 -0300

    get image resource implemented

--- src/main/java/io/github/guilhermebeneti1990/imageliteapi/application/images/ImagesController.java
+++ src/main/java/io/github/guilhermebeneti1990/imageliteapi/application/images/ImagesController.java
@@ -3,19 +3,21 @@ package io.github.guilhermebeneti1990.imageliteapi.application.images;
 import io.github.guilhermebeneti1990.imageliteapi.domain.entities.Image;
 import io.github.guilhermebeneti1990.imageliteapi.domain.enums.ImageExtension;
 import io.github.guilhermebeneti1990.imageliteapi.domain.services.ImageService;
+import jakarta.websocket.server.PathParam;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.io.IOException;
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -25,8 +27,14 @@ import java.util.List;
 public class ImagesController {
 
     private final ImageService service;
+    private final ImageMapper mapper;
 
     private static Logger logger = LoggerFactory.getLogger(ImagesController.class);
+
+    private URI buildImageUrl(Image image) {
+        String imagePath = "/" + image.getId();
+        return ServletUriComponentsBuilder.fromCurrentRequest().path(imagePath).build().toUri();
+    }
 
     @PostMapping
     public ResponseEntity save(
@@ -36,17 +44,29 @@ public class ImagesController {
     ) throws IOException {
         logger.info("Image Received -  name: {}, size: {}", file.getOriginalFilename(), file.getSize());
 
-        Image image = Image
-                .builder()
-                .name(name)
-                .tags(String.join(",", tags))
-                .size(file.getSize())
-                .extension(ImageExtension.valueOf(MediaType.valueOf(file.getContentType())))
-                .file(file.getBytes())
-                .build();
+        Image image = mapper.mapToImage(file, name, tags);
+        Image savedImage = service.save(image);
+        URI imageUri = buildImageUrl(savedImage);
 
-        service.save(image);
-
-        return ResponseEntity.ok().build();
+        return ResponseEntity.created(imageUri).build();
     }
+
+    @GetMapping("{id}")
+    public ResponseEntity<byte[]> getImage(@PathVariable String id) {
+        var imageExists = service.findById(id);
+        if(imageExists.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+
+        var image = imageExists.get();
+
+        HttpHeaders headers =  new HttpHeaders();
+        headers.setContentType(image.getExtension().getMediaType());
+        headers.setContentLength(image.getSize());
+        headers.setContentDispositionFormData("inline; filename=\"" + image.getFileName() + "\"", image.getFileName());
+
+        return new ResponseEntity<>(image.getFile(), headers, HttpStatus.OK);
+
+    }
+
 }
