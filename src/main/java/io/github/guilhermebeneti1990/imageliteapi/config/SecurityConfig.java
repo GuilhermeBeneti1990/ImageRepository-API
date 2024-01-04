27cf9dfdf23232b0117e48b82a02ff9ad860f195
GuilhermeBeneti1990 <guilhermebeneti@icloud.com>
2024-01-04 16:11:23 -0300

    image render permission fix

--- src/main/java/io/github/guilhermebeneti1990/imageliteapi/config/SecurityConfig.java
+++ src/main/java/io/github/guilhermebeneti1990/imageliteapi/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import io.github.guilhermebeneti1990.imageliteapi.config.interceptor.JwtIntercep
 import io.github.guilhermebeneti1990.imageliteapi.domain.services.UserService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -37,6 +38,7 @@ public class SecurityConfig {
                 .cors(cors -> cors.configure(http))
                 .authorizeHttpRequests(auth -> {
                     auth.requestMatchers("/v1/users/**").permitAll();
+                    auth.requestMatchers(HttpMethod.GET, "/v1/images/**").permitAll();
                     auth.anyRequest().authenticated();
                 })
                 .addFilterBefore(jwtInterceptor, UsernamePasswordAuthenticationFilter.class)
